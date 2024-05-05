d59f86acc01892a71c5f4e4bda3b2e1ebfd4c44f
Raz Aranyi <RazAranyi@gmail.com>
2024-05-05 17:11:39 +0300

    comments of the requestAndResponse package...

--- src/main/java/EasyAppointment/appointmentscheduler/requestsAndResponses/authentication/AuthenticationResponse.java
+++ src/main/java/EasyAppointment/appointmentscheduler/requestsAndResponses/authentication/AuthenticationResponse.java
@@ -1,23 +1,35 @@
 package EasyAppointment.appointmentscheduler.requestsAndResponses.authentication;
 
-import EasyAppointment.appointmentscheduler.models.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.catalina.connector.Response;
 
+/**
+ * This class represents the authentication response.
+ * It contains the token and message after successful authentication.
+ * It uses Lombok annotations for automatic generation of getters, setters, constructors, and builder.
+ */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthenticationResponse {
+    /**
+     * The token received after successful authentication.
+     */
     private String token;
+
+    /**
+     * The message received after successful authentication.
+     */
     private String message;
+
+    /**
+     * Constructor that initializes the token.
+     * @param token The token received after successful authentication.
+     */
     public AuthenticationResponse(String token) {
         this.token = token;
     }
-
 }
-
-
